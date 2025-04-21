46d81ad849b9d1b7ff42e706aeeb596d4f3295c4
Jakron <jakron.jd@gmail.com>
2025-04-21 16:14:43 +0300

    lib init

---
+++ libs/src/lib/core/core.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-core',
+  imports: [CommonModule],
+  templateUrl: './core.component.html',
+  styleUrl: './core.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CoreComponent {}
