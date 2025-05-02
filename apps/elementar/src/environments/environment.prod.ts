a0e3de8cda86f74926c4a8155daf913d39319096
Jakron <jakron.jd@gmail.com>
2025-05-02 23:26:06 +0300

    deploy5

---
+++ apps/elementar/src/environments/environment.prod.ts
@@ -0,0 +1,36 @@
+import config from '../../auth_config.json';
+
+const {
+  domain,
+  clientId,
+  authorizationParams: { audience },
+  apiUri,
+  errorPath,
+} = config as {
+  domain: string;
+  clientId: string;
+  authorizationParams: {
+    audience?: string;
+  };
+  apiUri: string;
+  errorPath: string;
+};
+
+export const environment = {
+  production: true,
+  siteUrl: 'http://localhost:4200',
+  googleAnalyticsId: '',
+  pageTitle: 'Elementar',
+  auth: {
+    domain,
+    clientId,
+    authorizationParams: {
+      ...(audience && audience !== '{yourApiIdentifier}' ? { audience } : null),
+      redirect_uri: window.location.origin,
+    },
+    errorPath,
+  },
+  httpInterceptor: {
+    allowedList: [`${apiUri}/*`],
+  },
+};
